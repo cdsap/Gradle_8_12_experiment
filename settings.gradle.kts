2366978bef992b726ba544a1f47090f8a7869feb
Inaki Villar <inaki.seri@gmail.com>
2025-01-02 15:23:15 -0800

    adding develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -42,6 +42,19 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
+plugins {
+    id("com.gradle.develocity") version "3.19"
+}
+ 
+develocity {
+    server = "https://ge.solutions-team.gradle.com/"
+    allowUntrustedServer = true
+    buildScan {
+        uploadInBackground.set(true)
+        publishing { true }
+    }
+}
 rootProject.name = "nowinandroid"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
